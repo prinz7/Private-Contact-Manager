ad2cc71a6463cc263bc001fc05bdfc24717a024e
Jannik Prinz <jannik.prinz@gmail.com>
2021-05-15 20:39:42 +0200

    Add project for desktop user interface.

---
+++ DesktopUI/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopUI
+{
+    /// <summary>
+    /// Interaktionslogik für Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+    public class TabSizeConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            TabControl tabControl = values[0] as TabControl;
+            double width = tabControl.ActualWidth / tabControl.Items.Count;
+            //Subtract 1, otherwise we could overflow to two rows.
+            return (width <= 1) ? 0 : (width - 1.4);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
--- PrivateContactManager/Program.cs
+++ PrivateContactManager/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using DesktopUI;
 
 namespace PrivateContactManager
 {
@@ -6,7 +8,20 @@ namespace PrivateContactManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread thread = new Thread(MainLoop);
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+        }
+
+        private static void MainLoop()
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            mainWindow.Closed += (sender2, e2) => mainWindow.Dispatcher.InvokeShutdown();
+
+            System.Windows.Threading.Dispatcher.Run();
         }
     }
 }
