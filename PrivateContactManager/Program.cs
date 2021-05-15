c2b73618814e3911af82290ab482ca15e4969b3b
Jannik Prinz <jannik.prinz@gmail.com>
2021-05-15 12:20:38 +0200

    Add initial project files.

---
+++ PrivateContactManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrivateContactManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
