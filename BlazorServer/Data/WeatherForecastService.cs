4181434df8acf3a89d1c3ed1a82cff3dd4e4178a
Jannik Prinz <jannik.prinz@gmail.com>
2022-03-06 18:34:27 +0100

    [Feature] Add blazor components package. Change title and color of the sidebar.

--- BlazorServer/Data/WeatherForecastService.cs
+++ BlazorServer/Data/WeatherForecastService.cs
@@ -9,12 +9,16 @@ namespace BlazorServer.Data
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = startDate.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            }).ToArray());
+            return Task.Run(() => {
+                Thread.Sleep(5000);
+                var x = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = startDate.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                }).ToArray();
+                return x;
+            });
         }
     }
 }
